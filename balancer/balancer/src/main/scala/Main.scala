2f72204b01e3480fd37d5e7d2857ac606d09eeb5
Victoria Pinzhen Liao <victorialiao1994@gmail.com>
2023-10-22 18:15:40 -0400

    update direction algo

--- balancer/balancer/src/main/scala/Main.scala
+++ balancer/balancer/src/main/scala/Main.scala
@@ -101,7 +101,7 @@ object SubwayCarRouting {
 
   def generateRoutingRecommendations(passengersList: List[Int]): Map[Int, (String, Int, Option[String])] = {
     passengersList.zipWithIndex.map { case (passengers, index) => val congestionLevel = getCongestionLevel(passengers)
-      val direction = getDirection(passengersList, index, passengers)
+      val direction = getBalancedDirection(passengersList, index)
       (index + 1) -> (congestionLevel, passengers, direction)
     }.toMap
   }
@@ -122,25 +122,35 @@ object SubwayCarRouting {
    * @return Optional direction recommendation: Some("Left"), Some("Right"), or None.
    *         Returns None if the current car is not crowded or if all cars are crowded.
    */
-  def getDirection(passengersList: List[Int], index: Int, passengers: Int): Option[String] = {
-    // Check if all cars are crowded
-    if (passengersList.forall(_ >= 60)) return None
 
-    // If the current car is not crowded, return None
-    if (passengers < 60) return None
 
-    // Find the nearest non-crowded car on the left
-    val leftIndex = (index - 1 to 0 by -1).find(passengersList(_) < 60)
-    // Find the nearest non-crowded car on the right
-    val rightIndex = (index + 1 until passengersList.length).find(passengersList(_) < 60)
+  def getBalancedDirection(passengersList: List[Int], index: Int): Option[String] = {
+    // If the number of passengers in the current car is less than or equal to 5, no adjustment is needed
+    if (passengersList(index) <= 5) return None
 
-    // Determine the direction based on the positions of the nearest non-crowded cars
+    // If at the ends of the train, check only the adjacent car
+    if (index == 0) {
+      if (passengersList(1) > passengersList(0)) return None
+    } else if (index == passengersList.length - 1) {
+      if (passengersList(index - 1) > passengersList(index)) return None
+    } else {
+      // If both adjacent cars have more passengers, return None
+      if (passengersList(index - 1) > passengersList(index) && passengersList(index + 1) > passengersList(index)) {
+        return None
+      }
+    }
+
+    // Find a car with fewer passengers on the left
+    val leftIndex = (index - 1 to 0 by -1).find(passengersList(_) < passengersList(index))
+    // Find a car with fewer passengers on the right
+    val rightIndex = (index + 1 until passengersList.length).find(passengersList(_) < passengersList(index))
+
     (leftIndex, rightIndex) match {
-      case (None, None) => None // If all adjacent cars are crowded, return None
-      case (Some(l), None) => Some("Left") // If only the left side has a non-crowded car, recommend Left
-      case (None, Some(r)) => Some("Right") // If only the right side has a non-crowded car, recommend Right
+      case (None, None) => None // If there are no cars with fewer passengers on both sides, return None
+      case (Some(l), None) => Some("Left") // If there is a car with fewer passengers only on the left, recommend going left
+      case (None, Some(r)) => Some("Right") // If there is a car with fewer passengers only on the right, recommend going right
       case (Some(l), Some(r)) =>
-        // If both sides have non-crowded cars, recommend the direction to the nearest one
+        // If there are cars with fewer passengers on both sides, recommend the direction to the nearest one
         if ((index - l) < (r - index)) Some("Left")
         else Some("Right")
     }
